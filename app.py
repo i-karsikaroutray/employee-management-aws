65cb6d612ee685e0e001bf0a84c656bd7405f369
Karsika Routray <karsikaroutray@gmail.com>
2026-06-12 17:40:09 +0530

    Added employee form submission

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,8 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/add-employee")
+@app.route("/add-employee", methods=["GET", "POST"])
 def add_employee():
+
+    if request.method == "POST":
+
+        employee_name = request.form["employee_name"]
+        email = request.form["email"]
+
+        return f"""
+        Employee Added Successfully!<br><br>
+        Name: {employee_name}<br>
+        Email: {email}
+        """
+
     return render_template("add_employee.html")
 
 if __name__ == "__main__":
