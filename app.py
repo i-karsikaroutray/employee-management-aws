257faae5310aa3e7c6ef94beae26b43931b982ff
Karsika Routray <karsikaroutray@gmail.com>
2026-06-12 17:06:08 +0530

    Added navigation and employee page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/add-employee")
+def add_employee():
+    return render_template("add_employee.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
