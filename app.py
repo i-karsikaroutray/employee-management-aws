3fbb4cde06ecea2f20170f1931f43b2b3a23ffae
Karsika Routray <karsikaroutray@gmail.com>
2026-06-12 18:23:01 +0530

    Added view employees page

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+employees = []
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -14,13 +16,21 @@ def add_employee():
         employee_name = request.form["employee_name"]
         email = request.form["email"]
 
-        return f"""
-        Employee Added Successfully!<br><br>
-        Name: {employee_name}<br>
-        Email: {email}
-        """
+        employees.append({
+            "name": employee_name,
+            "email": email
+        })
+
+        return "Employee Added Successfully!"
 
     return render_template("add_employee.html")
+
+@app.route("/view-employees")
+def view_employees():
+    return render_template(
+        "view_employees.html",
+        employees=employees
+    )    
 
 if __name__ == "__main__":
     app.run(debug=True)
