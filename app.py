946af0a40a8856bb0d4fdc1bf51c6e26c64b9c3e
Karsika Routray <karsikaroutray@gmail.com>
2026-06-14 16:58:41 +0530

    Added MySQL integration and environment variables

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from dotenv import load_dotenv
+import os
+import mysql.connector
 
+load_dotenv()
 app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+cursor = db.cursor()
 
 employees = []
 
@@ -13,24 +26,30 @@ def add_employee():
 
     if request.method == "POST":
 
-        employee_name = request.form["employee_name"]
+        name = request.form["employee_name"]
         email = request.form["email"]
 
-        employees.append({
-            "name": employee_name,
-            "email": email
-        })
+        sql = "INSERT INTO employees (name, email) VALUES (%s, %s)"
+        values = (name, email)
 
-        return "Employee Added Successfully!"
+        cursor.execute(sql, values)
+        db.commit()
+
+        return redirect("/view-employees")
 
     return render_template("add_employee.html")
 
 @app.route("/view-employees")
 def view_employees():
+
+    cursor.execute("SELECT * FROM employees")
+    employees = cursor.fetchall()
+
     return render_template(
         "view_employees.html",
         employees=employees
-    )    
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
+
